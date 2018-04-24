cb0551111895ca1efac55b441551c21bc37f953f
Deva Kumar <deva.kumar@sas.com>
2018-04-24 01:06:53 -0400

    tested version against uidemos

--- src/iService.js
+++ src/iService.js
@@ -19,22 +19,22 @@
 
 'use strict';
 
-let debug          = require('debug');
-let debugProxy     = debug('proxy');
-let debugRouter    = debug('router');
-let proxyLogger    = debug('proxylogger');
+let debug = require('debug');
+let debugProxy = debug('proxy');
+let debugRouter = debug('router');
+let proxyLogger = debug('proxylogger');
 let responseLogger = debug('response');
-let server       = require('./server');
-let boom         = require('boom');
-let request      = require('request');
+let server = require('./server');
+let boom = require('boom');
+let request = require('request');
 /* require( 'request-debug' )( request ); */
-let os           = require('os');
-let v8           = require('v8');
-let SASauth      = require('./SASauth');
-let qs           = require('qs');
-let uuid         = require('uuid');
+let os = require('os');
+let v8 = require('v8');
+let SASauth = require('./SASauth');
+let qs = require('qs');
+let uuid = require('uuid');
 
-module.exports = function iService (uTable, useDefault , asset, rootHandler){
+module.exports = function iService(uTable, useDefault, asset, rootHandler) {
 
     process.env.APPHOST = (process.env.APPHOST === '*') ? os.hostname() : process.env.APPHOST;
     let appName = '/' + process.env.APPNAME;
@@ -42,8 +42,8 @@ module.exports = function iService (uTable, useDefault , asset, rootHandler){
     let auth2 = {};
     let handleOthers;
 
-    if (process.env.PROXYSERVER === 'YES'){
-        process.env.OAUTH2='YES';
+    if (process.env.PROXYSERVER === 'YES') {
+        process.env.OAUTH2 = 'YES';
     }
     console.log(`appName ${appName}`);
     console.log(`asset ${asset} `);
@@ -55,7 +55,7 @@ module.exports = function iService (uTable, useDefault , asset, rootHandler){
             strategy: 'sas'
         };
         auth2 = {
-            mode    : 'required',
+            mode: 'required',
             strategy: 'session'
         };
         auth2 = false;
@@ -64,64 +64,56 @@ module.exports = function iService (uTable, useDefault , asset, rootHandler){
         auth1 = false;
         auth2 = false;
     }
-  
+
     let defaultTable =
-            [ {
-                method: [ 'GET' ],
-                path  : `${appName}`,
-                config: {
-                    auth   : auth1,
-                    handler: getApp
-                }
-            }, {
-                method: [ 'GET' ],
-                path  : `${appName}/{param*}`,
-                config: {
-                    handler: getApp2
-                }
+        [{
+            method: ['GET'],
+            path: `${appName}`,
+            config: {
+                auth: auth1,
+                handler: getApp
+            }
+        }, {
+            method: ['GET'],
+            path: `${appName}/{param*}`,
+            config: {
+                handler: getApp2
+            }
 
-            }, {
-                method: [ 'GET' ],
-                path  : `${appName}/callback${appName}`,
-                config: {
-                    handler: AppCallback
-                }
+        }, {
+            method: ['GET'],
+            path: `${appName}/callback${appName}`,
+            config: {
+                handler: AppCallback
+            }
 
-            }, {
-                method: [ 'GET' ],
-                path  : `/shared/{param*}`,
-                config: {
-                    handler: getShared
-                }
-            }, {
-                method: [ 'GET' ],
-                path  : `/restafServerInfo`,
-                config: {
-                    auth   : false,
-                    handler: serverInfo
-                }
-            },{
-                method: [ 'GET' ],
-                path  : '/favicon.ico',
-                config: {
-                    auth   : false,
-                    handler: getIcon
-                }
-            }, {
-                method: [ 'GET' ],
-                path  : '/testserver',
-                config: {
-                    handler: testServer
-                }
+        }, {
+            method: ['GET'],
+            path: `/shared/{param*}`,
+            config: {
+                handler: getShared
+            }
+        }, {
+            method: ['GET'],
+            path: `/restafServerInfo`,
+            config: {
+                auth: false,
+                handler: serverInfo
+            }
+        }, {
+            method: ['GET'],
+            path: '/favicon.ico',
+            config: {
+                auth: false,
+                handler: getIcon
             }
         }, {
             method: ['GET'],
             path: '/testserver',
             config: {
-                auth: auth2,
                 handler: testServer
             }
-        }
+         }
         ];
 
     // Tried payload.parse = false -- way too much code to handle payload
@@ -133,13 +125,13 @@ module.exports = function iService (uTable, useDefault , asset, rootHandler){
                 handler: handleProxy
             }
         };
-        defaultTable = [...defaultTable, handleOthers];
+        defaultTable = [ ...defaultTable, handleOthers ];
     } else {
         handleOthers = {
             method: ['GET'],
             path: '/{param*}',
             config: {
-                auth   : false,
+                auth: false,
                 handler: getApp2
             }
         };
@@ -149,7 +141,7 @@ module.exports = function iService (uTable, useDefault , asset, rootHandler){
     let userRouterTable;
     if (uTable !== null) {
         if (useDefault === true) {
-            userRouterTable = [ ...defaultTable, ...uTable ];
+            userRouterTable = [...defaultTable, ...uTable];
         } else {
             userRouterTable = [...uTable];
         }
@@ -165,9 +157,9 @@ module.exports = function iService (uTable, useDefault , asset, rootHandler){
 //
 // Had to add cache to handle Edge browser - must be a way not to have to do this.
 //
-async function testServer (req, h) {
+async function testServer(req, h) {
     debugger;
-    let token ;
+    let token;
     let url = 'testserver.html';
     console.log(req.auth.credentials);
     return h.file(url);
@@ -176,44 +168,44 @@ async function testServer (req, h) {
 
 
 
-async function getApp (req, h) {
+async function getApp(req, h) {
     debugger;
     if (process.env.PROXYSERVER === 'YES') {
-        return getAuthApp(null, req,h)
-    } else { 
+        return getAuthApp(null, req, h)
+    } else {
         let indexHTML = (process.env.APPENTRY == null) ? 'index.html' : process.env.APPENTRY;
         return h.file(indexHTML);
     }
 }
 
-async function getAuthApp (rootHandler, req, h) {
+async function getAuthApp(rootHandler, req, h) {
     debugger;
     const sid = uuid.v4();
-    await req.server.app.cache.set(sid,  req.auth.credentials) ;
-    req.cookieAuth.set({sid});
+    await req.server.app.cache.set(sid, req.auth.credentials);
+    req.cookieAuth.set({ sid });
     let indexHTML = (process.env.APPENTRY == null) ? 'index.html' : process.env.APPENTRY;
     return h.file(`${indexHTML}`);
 }
 
-async function handleProxy (req, h) {
+async function handleProxy(req, h) {
     debugger;
     let token;
     try {
-        token  = await getToken(req, h);
+        token = await getToken(req, h);
         debugger;
         let proxyResponse = await handleProxyRequest(req, h, token);
-       
+
         let response = h.response(proxyResponse.body);
-        for (let hkey in proxyResponse.headers){
+        for (let hkey in proxyResponse.headers) {
             response.header(hkey, proxyResponse.headers[hkey]);
         }
         return response;
     }
-    catch(err) {
+    catch (err) {
         return boom.unauthorized(err)
     }
 }
-async function getToken (req, h) {
+async function getToken(req, h) {
     debugger;
     if (req.auth.credentials !== null) {
         debugger;
@@ -226,13 +218,13 @@ async function getToken (req, h) {
         return sid.credentials;
     }
 }
-function handleProxyRequest (req, h, token) {
+function handleProxyRequest(req, h, token) {
     return new Promise((resolve, reject) => {
         debugger;
-        let uri   = `${process.env.SAS_PROTOCOL}${process.env.VIYA_SERVER}/${req.params.params}`;
+        let uri = `${process.env.SAS_PROTOCOL}${process.env.VIYA_SERVER}/${req.params.params}`;
         let headers = { ...req.headers };
         delete headers.host;
-        delete headers[ 'user-agent' ];
+        delete headers['user-agent'];
         delete headers.origin;
         delete headers.referer;
         delete headers.connection;
@@ -241,11 +233,11 @@ function handleProxyRequest (req, h, token) {
         }
         debugger;
         let config = {
-            url    : uri,
-            method : req.method,
+            url: uri,
+            method: req.method,
             headers: headers,
-            gzip   : true,
-            auth   : {
+            gzip: true,
+            auth: {
                 bearer: token
             }
         };
@@ -266,22 +258,22 @@ function handleProxyRequest (req, h, token) {
 
         debugProxy(JSON.stringify(config, null, 4));
         proxyLogger(config.url);
-        request(config, (err, response, body) =>  {
+        request(config, (err, response, body) => {
             debugger;
             if (err) {
-                reject(err); 
+                reject(err);
             } else {
                 debugger;
-                responseLogger({url: `------------------------------------------${config.url}`});
+                responseLogger({ url: `------------------------------------------${config.url}` });
                 responseLogger(req.query);
-                responseLogger((typeof body === 'string' ? {body: body} : body));
+                responseLogger((typeof body === 'string' ? { body: body } : body));
                 if (response.headers.hasOwnProperty('content-encoding')) {
-                    delete response. headers['content-encoding'];
+                    delete response.headers['content-encoding'];
                 }
                 responseLogger(response.headers['content-coding']);
                 debugger;
-                resolve({headers: response.headers, body: body});
-        
+                resolve({ headers: response.headers, body: body });
+
             }
         });
     });
@@ -292,7 +284,8 @@ function handleProxyRequest (req, h, token) {
 
 
 
-async function AppCallback (req, h) {
+async function AppCallback(req, h) {
+    debugger;
     proxyLogger('In callback');
     let indexHTML = (process.env.APPENTRY == null) ? 'index.html' : process.env.APPENTRY;
     return h.file(`${indexHTML}`);
@@ -302,22 +295,22 @@ async function AppCallback (req, h) {
 // get app server files
 //
 
-async function getIcon (req, h) {
+async function getIcon(req, h) {
     debugger;
     return h.file('favicon.ico');
 }
 
-async function getApp2 (req, h) {
+async function getApp2(req, h) {
     return h.file(req.params.param);
 }
 
-async function getShared (req, h) {
+async function getShared(req, h) {
     debugger;
     return h.file(`shared/${req.params.param}`);
 }
-async function serverInfo (req, h) {
+async function serverInfo(req, h) {
     debugger;
-    let protocol = (process.env.SAS_SSL_ENABLED === 'YES') ? 'https://' :'http://';
+    let protocol = (process.env.SAS_SSL_ENABLED === 'YES') ? 'https://' : 'http://';
     let js = `let VIYA_HOST   = "${protocol}${process.env.VIYA_SERVER}";`;
     return js;
 }
