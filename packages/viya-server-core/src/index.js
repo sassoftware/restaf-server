91ae0cbe88a6a53806cf52b1e2c3cc04f3224abf
Deva Kumar <deva.kumar@sas.com>
2022-03-11 15:01:33 -0500

    turned off swagger due to package issues

--- packages/viya-server-core/src/index.js
+++ packages/viya-server-core/src/index.js
@@ -33,7 +33,7 @@ module.exports = function core (uTable, useDefault, serverMode, customize) {
     useDefault = true;
   }
   console.log('Initialization started ============================================================');
-  console.log('version: 1.3.2');
+  console.log('version: 1.3.4');
   console.log(
     `\nConfiguration:
           Dockerfile: ${docker}
