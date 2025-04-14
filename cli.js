cefa4e36720eababbe3fe7b72a20e6429bfdeceb
Deva <deva.kumar@sas.com>
2025-04-14 10:55:18 -0400

    save

--- cli.js
+++ cli.js
@@ -5,5 +5,5 @@
 */
 //let appServer = require('./lib/index.js');
 let appServer = require('./lib/index.js');
-console.log('Starting the cli for @sassoftware/viya-appserverjs');
+console.log('Starting the cli for @sassoftware/viya-serverjs');
 appServer(null, true, 'app', null);
