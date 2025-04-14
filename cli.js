47ae98b2fe660507203c2861399cf0e2a53da759
Deva <deva.kumar@sas.com>
2025-04-14 08:51:46 -0400

    fix server.js

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /*
-* Copyright © 2019, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+* Copyright © 2025, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 * SPDX-License-Identifier: Apache-2.0
 */
 //let appServer = require('./lib/index.js');
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 /*
- * Copyright © 2019, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+ * Copyright © 2025, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
-let core = require('./lib/index.js');
+let appServer = require('./lib/index.js');
 debugger;
-core(getCustomHandler, true, 'app', customize);
+appServer(getCustomHandler, true, 'app', customize);
 function customize(key, _options) {
   debugger;
   let info = {
